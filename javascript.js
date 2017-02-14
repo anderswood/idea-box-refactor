ef7774207239c9639b57f36baaa9f74bf0731325
Anders Wood <anderswood@gmail.com>
2017-02-13 20:07:35 -0700

    start working on object constructor functionwith random number for ID- in process

--- javascript.js
+++ javascript.js
@@ -1,29 +1,46 @@
-var titleInput = $('#title');
-var bodyInput = $('#body');
+var storageArray = []
 
 $('#save').on('click', function() {
-
 
   // check inputs - confirm inputs are populated | disable 'save' button
 
   //create object | constructor function
 
   //store it
-
+  storeIdea();
 
 
   addIdea();
-
-// The text fields should be cleared and ready to accept a new idea.
-clearInputs();
+  clearInputs();
 });
+
+function storeIdea(){
+  var id = Math.floor(Math.random()*1e10)
+  var title = $('#title').val();
+  var body = $('#body').val();
+  var newIdea = new Idea(id,title,body);
+  storageArray.push(newIdea);
+
+
+
+  //stringify
+
+  //setItem
+}
+
+function Idea(id,title,body) {
+  this.id = id
+  this.title = title;
+  this.body = body;
+  this.quality = 'swill';
+}
 
 function addIdea() {
   $('.ideas').prepend(
     `<article class="template">
-      <h2>${titleInput.val()}</h2>
+      <h2>${$('#title').val()}</h2>
       <img id="delete-btn" src="" alt="delete button">
-      <p>${bodyInput.val()}</p>
+      <p>${$('#body').val()}</p>
       <img src="" alt="upvote button">
       <img src="" alt="downvote button">
       <h3>quality: <span id="quality"></span></h3>
@@ -34,9 +51,9 @@ function clearInputs() {
   $('form')[0].reset();
   };
 
-
 $('.ideas').on('click', '#delete-btn', function() {
   $(this).parent().remove('article');
+
 
 //   //remove from storage
 
