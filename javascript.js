6809fe15733ae282699c5244b7dd95f4112e3e50
Anders Wood <anderswood@gmail.com>
2017-02-14 14:47:59 -0700

    add localstorage capability with stringify and parse

--- javascript.js
+++ javascript.js
@@ -1,22 +1,26 @@
+
 var storageArray = JSON.parse(localStorage.getItem('storeMe')) || [];
 
 $(document).ready(function() {
-  //loop through storageArray, for each thing in it
+  storageArray.forEach( function(idea){
+    var id = idea.id;
+    var title = idea.title;
+    var body = idea.body;
+    var quality = idea.quality;
+    printIdea(title,body);
+  })
 });
 
 $('#save').on('click', function() {
 
   // check inputs - confirm inputs are populated | disable 'save' button
 
-  //create object | constructor function
-
-  //store it
   storeIdea();
-  pushToStorage()
-
-  addIdea();
+  pushToStorage();
+  printIdea($('#title').val(),$('#body').val());
   clearInputs();
 });
+
 
 function storeIdea(){
   var id = Math.floor(Math.random()*1e10)
@@ -24,48 +28,45 @@ function storeIdea(){
   var body = $('#body').val();
   var newIdea = new Idea(id,title,body);
   storageArray.push(newIdea);
-
-  //stringify
-
-  //setItem
-}
-
-function pushToStorage() {
-  localStorage.setItem("storeMe", JSON.stringify(storageArray));
-}
-
-function pullFromStorage() {
-  JSON.parse(localStorage.getItem('storeMe'));
-
-}
+};
 
 function Idea(id,title,body) {
   this.id = id;
   this.title = title;
   this.body = body;
   this.quality = 'swill';
-}
+};
 
-function addIdea() {
+function pushToStorage() {
+  localStorage.setItem("storeMe", JSON.stringify(storageArray));
+};
+
+function printIdea(title,body) {
   $('.ideas').prepend(
     `<article class="template">
-      <h2>${$('#title').val()}</h2>
-      <img id="delete-btn" src="" alt="delete button">
-      <p>${$('#body').val()}</p>
-      <img src="" alt="upvote button">
-      <img src="" alt="downvote button">
-      <h3>quality: <span id="quality"></span></h3>
+    <h2>${title}</h2>
+    <img id="delete-btn" src="" alt="delete button">
+    <p>${body}</p>
+    <img src="" alt="upvote button">
+    <img src="" alt="downvote button">
+    <h3>quality: <span id="quality"></span></h3>
     </article>`);
-}
+};
 
 function clearInputs() {
   $('form')[0].reset();
-  };
+};
+
+function pullFromStorage() {
+  JSON.parse(localStorage.getItem('storeMe'));
+};
+
 
 $('.ideas').on('click', '#delete-btn', function() {
+ //store ID in local variable
+ var id = $('#id')
   $(this).parent().remove('article');
 
-
-//   //remove from storage
-
+//take ID, filter through StorageArray and remove object with matching ID from storage
+// run filter function on storagearray, match ID from deleted Idea with ID of object in array, delete the filtered object
 });
